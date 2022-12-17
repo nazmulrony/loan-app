811d4c32cacf1ce783baf2f06de5d6cf755b08c3
nazmulrony <nhrony65@gmail.com>
2022-12-17 10:57:07 +0600

    form validation patters added

--- src/components/LoanForm.js
+++ src/components/LoanForm.js
@@ -8,23 +8,39 @@ const LoanForm = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
     const [birthDate, setBirthDate] = useState(new Date());
     const [toggleState, setToggleState] = useState(1);
+    //form handle 
     const handleSaveLoanData = (data) => {
+        console.log('Button clicked');
         const dateOfBirth = birthDate.toLocaleDateString();
+        const loanData = {
+            name: `${data.firstName} ${data.lastName}`,
+            dateOfBirth: dateOfBirth,
+            phone: data.phone,
+            businessName: data.businessName,
+            address: data.address,
+            gstNo: data.gstNo,
+            email: data.email,
+            loanAmount: data.amount,
+            interestRate: data.interestRate,
+            loanTenure: data.loanTenure,
+            income: data.income
+        }
+        console.log(loanData);
 
 
     }
     return (
-        <div className='mx-auto w-2/3 mt-10 bg-gray-200 border'>
+        <div className='mx-auto lg:w-2/3 mt-10 bg-gray-200 border'>
             <div className='flex gap-6'>
-                <div onClick={() => setToggleState(1)} className={`cursor-pointer px-6 py-2 ${toggleState === 1 ? 'bg-gray-100  border-t-2 border-blue-600' : ''} `}>Tab 1</div>
-                <div onClick={() => setToggleState(2)} className={`cursor-pointer px-6 py-2 ${toggleState === 2 ? 'bg-gray-100  border-t-2 border-blue-600' : ''}`}>Tab 2</div>
-                <div onClick={() => setToggleState(3)} className={`cursor-pointer px-6 py-2 ${toggleState === 3 ? 'bg-gray-100  border-t-2 border-blue-600' : ''}`}>Tab 3</div>
+                <div onClick={() => setToggleState(1)} className={`cursor-pointer px-6 py-2 ${toggleState === 1 ? 'bg-gray-50  border-t-2 border-blue-600' : ''} `}>Tab 1</div>
+                <div onClick={() => setToggleState(2)} className={`cursor-pointer px-6 py-2 ${toggleState === 2 ? 'bg-gray-50  border-t-2 border-blue-600' : ''}`}>Tab 2</div>
+                <div onClick={() => setToggleState(3)} className={`cursor-pointer px-6 py-2 ${toggleState === 3 ? 'bg-gray-50  border-t-2 border-blue-600' : ''}`}>Tab 3</div>
             </div>
 
-            <form onSubmit={handleSubmit(handleSaveLoanData)} className="bg-gray-100 flex flex-col items-start px-10 py-6 ">
+            <form onSubmit={handleSubmit(handleSaveLoanData)} className="bg-gray-50 flex flex-col items-start px-10 py-6 ">
                 {/* First tab Code */}
                 <div className={`p-2 w-full ${toggleState === 1 ? 'block' : 'hidden'}`}>
-                    <div className=' flex gap-6 justify-between w-full mb-4'>
+                    <div className=' flex flex-col lg:flex-row gap-6 justify-between w-full mb-4'>
                         <div className='flex w-full flex-col items-start'>
                             <label className="label">First Name</label>
                             <input
@@ -33,7 +49,7 @@ const LoanForm = () => {
                                     required: 'First name is required'
                                 })}
                                 placeholder="Enter your first name"
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md"
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
                             />
                             {errors.firstName && <p className='text-red-600 mt-1 text-sm'>{errors.firstName?.message}</p>}
                         </div>
@@ -45,44 +61,41 @@ const LoanForm = () => {
                                     required: 'Last name is required'
                                 })}
                                 placeholder="Enter your last name"
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md "
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border "
                             />
                             {errors.lastName && <p className='text-red-600 mt-1 text-sm'>{errors.lastName?.message}</p>}
                         </div>
                     </div>
-                    <div className=' flex gap-6 justify-between w-full'>
+                    <div className=' flex flex-col lg:flex-row gap-6 justify-between w-full'>
                         <div className='flex w-full flex-col items-start'>
                             <label className="label">Phone Number</label>
                             <input
-                                type="number"
+                                type="text"
                                 {...register("phone", {
-                                    required: 'Phone number is required'
+                                    required: 'Phone number is required',
+                                    pattern: { value: /^(\+\d{1,3}[- ]?)?\d{10}$/, message: 'Invalid phone number' }
                                 })}
                                 placeholder="Enter your phone number"
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md"
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
                             />
                             {errors.phone && <p className='text-red-600 mt-1 text-sm'>{errors.phone?.message}</p>}
                         </div>
                         <div className='flex w-full flex-col items-start'>
                             <label className="label">Date of Birth</label>
                             <DatePicker
-                                {...register("dateOfBirth", {
-                                    required: 'Date of birth is required'
-                                })}
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md"
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
                                 selected={birthDate}
                                 onChange={(date) => setBirthDate(date)}
                             />
-                            {errors.dateOfBirth && <p className='text-red-600 mt-1 text-sm'>{errors.dateOfBirth?.message}</p>}
                         </div>
                     </div>
                     <div className='flex justify-end mt-6'>
-                        <button onClick={() => setToggleState(toggleState + 1)} className='px-6 py-2 bg-blue-600 rounded-lg text-white flex items-center hover:bg-blue-700 duration-300'>Next <AiOutlineDoubleRight className='text-xl text-white' /></button>
+                        <label onClick={() => setToggleState(toggleState + 1)} className='px-6 py-2 bg-blue-600 rounded-lg text-white flex items-center hover:bg-blue-700 duration-300'>Next <AiOutlineDoubleRight className='text-xl text-white' /></label>
                     </div>
                 </div>
                 {/* Second Tab Code */}
                 <div className={`p-2 w-full  ${toggleState === 2 ? 'block' : 'hidden'}`}>
-                    <div className=' flex gap-6 justify-between w-full mb-4'>
+                    <div className=' flex flex-col lg:flex-row gap-6 justify-between w-full mb-4'>
                         <div className='flex w-full flex-col items-start'>
                             <label className="label">Business Name</label>
                             <input
@@ -91,7 +104,7 @@ const LoanForm = () => {
                                     required: 'Business name is required'
                                 })}
                                 placeholder="Enter your business name"
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md"
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
                             />
                             {errors.businessName && <p className='text-red-600 mt-1 text-sm'>{errors.businessName?.message}</p>}
                         </div>
@@ -100,15 +113,16 @@ const LoanForm = () => {
                             <input
                                 type="text"
                                 {...register("gstNo", {
-                                    required: 'GST number is required'
+                                    required: 'GST number is required',
+                                    minLength: { value: 15, message: 'GST number must 15 characters long.' }
                                 })}
                                 placeholder="Enter GST number"
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md "
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
                             />
                             {errors.gstNo && <p className='text-red-600 mt-1 text-sm'>{errors.gstNo?.message}</p>}
                         </div>
                     </div>
-                    <div className=' flex gap-6 justify-between w-full'>
+                    <div className=' flex flex-col lg:flex-row gap-6 justify-between w-full'>
                         <div className='flex w-full flex-col items-start'>
                             <label className="label">Address</label>
                             <input
@@ -117,7 +131,7 @@ const LoanForm = () => {
                                     required: 'Address is required'
                                 })}
                                 placeholder="Enter your address"
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md"
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
                             />
                             {errors.address && <p className='text-red-600 mt-1 text-sm'>{errors.address?.message}</p>}
                         </div>
@@ -126,21 +140,22 @@ const LoanForm = () => {
                             <input
                                 type="email"
                                 {...register("email", {
-                                    required: 'Email is required'
+                                    required: 'Email is required',
+                                    pattern: { value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/, message: 'Invalid email' }
                                 })}
                                 placeholder="Enter your email"
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md "
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
                             />
                             {errors.email && <p className='text-red-600 mt-1 text-sm'>{errors.email?.message}</p>}
                         </div>
                     </div>
                     <div className='flex justify-end mt-6'>
-                        <button onClick={() => setToggleState(toggleState + 1)} className='px-6 py-2 bg-blue-600 rounded-lg text-white flex items-center hover:bg-blue-700 duration-300'>Next <AiOutlineDoubleRight className='text-xl text-white' /></button>
+                        <label onClick={() => setToggleState(toggleState + 1)} className='px-6 py-2 bg-blue-600 rounded-lg text-white flex items-center hover:bg-blue-700 duration-300'>Next <AiOutlineDoubleRight className='text-xl text-white' /></label>
                     </div>
                 </div>
                 {/* Third tab code */}
                 <div className={`p-2 w-full  ${toggleState === 3 ? 'block' : 'hidden'}`}>
-                    <div className=' flex gap-6 justify-between w-full mb-4'>
+                    <div className=' flex flex-col lg:flex-row gap-6 justify-between w-full mb-4'>
                         <div className='flex w-full flex-col items-start'>
                             <label className="label">Loan Amount</label>
                             <input
@@ -149,24 +164,34 @@ const LoanForm = () => {
                                     required: 'Business name is required'
                                 })}
                                 placeholder="Enter your business name"
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md"
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
                             />
                             {errors.amount && <p className='text-red-600 mt-1 text-sm'>{errors.amount?.message}</p>}
                         </div>
                         <div className='flex w-full flex-col items-start'>
                             <label className="label">Interest Rate</label>
-                            <input
+                            {/* <input
                                 type="text"
                                 {...register("interestRate", {
                                     required: 'Interest rate is required'
                                 })}
                                 placeholder="Enter interest rate"
                                 className="p-2 w-full shadow-md shadow-black/5 rounded-md "
-                            />
+                            /> */}
+                            <select
+                                {...register("interestRate", {
+                                    required: 'Interest rate is required'
+                                })}
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
+                            >
+                                <option value="5">5%</option>
+                                <option value="10">10%</option>
+                                <option value="15">15%</option>
+                            </select>
                             {errors.interestRate && <p className='text-red-600 mt-1 text-sm'>{errors.interestRate?.message}</p>}
                         </div>
                     </div>
-                    <div className=' flex gap-6 justify-between w-full'>
+                    <div className=' flex flex-col lg:flex-row gap-6 justify-between w-full'>
                         <div className='flex w-full flex-col items-start'>
                             <label className="label">Income</label>
                             <input
@@ -175,7 +200,7 @@ const LoanForm = () => {
                                     required: 'Income is required'
                                 })}
                                 placeholder="Enter your income"
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md"
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
                             />
                             {errors.income && <p className='text-red-600 mt-1 text-sm'>{errors.income?.message}</p>}
                         </div>
@@ -183,17 +208,18 @@ const LoanForm = () => {
                             <label className="label">Loan Tenure</label>
                             <input
                                 type="text"
-                                {...register("income", {
-                                    required: 'Loan tenure is required'
+                                {...register("loanTenure", {
+                                    required: 'Loan tenure is required',
+                                    pattern: { value: /^[1-9]\d*(\.\d+)?$/, message: 'Invalid loan tenure' }
                                 })}
                                 placeholder="Enter loan tenure (years)"
-                                className="p-2 w-full shadow-md shadow-black/5 rounded-md"
+                                className="p-2 w-full shadow-md shadow-black/5 rounded-md border"
                             />
                             {errors.loanTenure && <p className='text-red-600 mt-1 text-sm'>{errors.loanTenure?.message}</p>}
                         </div>
                     </div>
                     <div className='flex justify-end mt-6'>
-                        <button type='submit' className='px-6 py-2 bg-blue-600 rounded-lg text-white flex items-center hover:bg-blue-700 duration-300'>Submit </button>
+                        <input type='submit' value="Submit" className='px-6 py-2 cursor-pointer bg-blue-600 rounded-lg text-white flex items-center hover:bg-blue-700 duration-300' />
                     </div>
                 </div>
             </form>
